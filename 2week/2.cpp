1e274f57586da367802877aeffa74e10f749f086
LeeJaeJun1 <jj030207@naver.com>
2025-02-17 17:31:08 +0900

    2week

--- 2week/2.cpp
+++ 2week/2.cpp
@@ -1,19 +1,27 @@
 #include<iostream>
 using namespace std;
 
-// 자연수 저장하는 단일 연결 리스트 2개 생성
-// L1, L2를 만들고 병합해서 L3를 만들고 특정 값 e를 찾은 다음, 그 노드 앞에 v를 포함하는 노드 추가하고 출력
-
-struct Node{
+class node{
+private:
     int data;
-    Node* next;
+    node* next;
+public:
+    node() {
+        this->data = 0;
+        this->next = 0;
+    }
+    node(int x) {
+        data = x;
+        next = nullptr;
+    }
+    friend class LinkedList;
 };
 
 class LinkedList {
 private:
-    Node* head;
-    Node* tail;
     int listSize;
+    node* head;
+    node* tail;
 public:
     LinkedList() {
         head = NULL;
@@ -21,28 +29,21 @@ public:
         listSize = 0;
     }
 
-    Node* hd() {
-        return head;
-    }
-
-    Node* tl() {
-        return tail;
-    }
-
-    void apd(Node* n) {
-        tail->next = n;
-    }
-
-    int rsize() {
-        return listSize;
-    }
-
-    void changesize(int k) {
-        listSize+= k;
+    bool empty() {
+        if(listSize==0) {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     void Print() {
-        Node* curNode = head;
+        if(empty()) {
+            cout << "empty" << endl;
+            return;
+        }
+        node* curNode = head;
         while(curNode != NULL) {
             cout << curNode->data << " ";
             curNode = curNode->next;
@@ -50,81 +51,79 @@ public:
         cout << endl;
     }
 
-    bool empty() {
-        if(listSize == 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    void Append(int data) {
-        Node* newNode = new Node;
-        newNode-> data = data;
-        newNode->next = NULL;
+    void Append(int x) {
+        node* newNode = new node(x);
         if(empty()) {
             head = tail = newNode;
         }
-        else{
+        else {
             tail->next = newNode;
             tail = newNode;
         }
         listSize++;
-        return;
     }
 
-    void add(int e, int v) {
-        Node* newNode = new Node;
-        newNode->data = v;
-        Node* curNode = head;
-        if(e == curNode->data) { // e값이 헤드일 때
-            newNode->next = head;
-            head = newNode;
-        }
-        else {
-            Node *preNode = head;
-            for (int i = 0; i < listSize; i++) {
-                if (e == curNode->data) {
-                    preNode->next = newNode;
-                    newNode->next = curNode;
+    void insert(int e, int v) {
+        node* curNode = head;
+        node* preNode = head;
+        node* addNode = new node(v);
+        while(curNode!=NULL) {
+            if (curNode->data == e) {
+                if (curNode == head) {
+                    addNode->next = head;
+                    head = addNode;
+                    Print();
+                    return;
                 }
-                preNode = curNode;
-                curNode = curNode->next;
+                else {
+                    preNode->next = addNode;
+                    addNode->next = curNode;
+                    Print();
+                    return;
+                }
             }
+            preNode = curNode;
+            curNode = curNode->next;
         }
-        listSize++;
-        Print();
-        return;
+    }
+
+    void merge(LinkedList* l1, LinkedList* l2) {
+        head = l1->head;
+        node* curNode = head;
+        while(curNode->next != NULL) {
+            curNode = curNode->next;
+        }
+        curNode->next = l2->head;
+        while(curNode->next != NULL) {
+            curNode = curNode->next;
+        }
+        tail = curNode;
+        curNode = head;
+        listSize = l1->listSize + l2->listSize;
     }
 };
 
 int main() {
-    int m = 0;
-    int x,e,v,n;
-    int M; cin >> M;
+    int M,e,v;
+    cin >> M;
     while(M--) {
-        LinkedList list1; LinkedList list2;
-
-        cin >> n;
-        while(n--) {
-            cin >> x;
-            list1.Append(x);
+        LinkedList l1;
+        LinkedList l2;
+        LinkedList l3;
+        int n1, num1, n2, num2;
+        cin >> n1;
+        for(int i = 0; i < n1; i++) {
+            cin >> num1;
+            l1.Append(num1);
         }
-
-        cin >> n;
-        while(n--) {
-            cin >> x;
-            list2.Append(x);
+        cin >> n2;
+        for(int j = 0; j < n2; j++) {
+            cin >> num2;
+            l2.Append(num2);
         }
-
-        Node* curNode = list2.hd();
-        list1.apd(curNode);
-        list1.changesize(list2.rsize());
-        list1.Print();
-
+        l3.merge(&l1, &l2);
+        l3.Print();
         cin >> e >> v;
-        list1.add(e,v);
+        l3.insert(e,v);
     }
-    return 0;
 }
