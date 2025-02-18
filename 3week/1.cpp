48a05f16c649de6971e86aa4f1c4410d1e36c985
LeeJaeJun1 <jj030207@naver.com>
2025-02-18 11:31:36 +0900

    3week

--- 3week/1.cpp
+++ 3week/1.cpp
@@ -1,103 +1,98 @@
 #include<iostream>
-#include<string>
 using namespace std;
 
 class arrayStack {
 private:
-    int *arr;
-    int capacity; // 최대 크기
+    int capacity;
     int topIndex;
+    int *arr;
 public:
-    arrayStack(int capacity);
-    bool empty();
-    int size();
-    int top();
-    void push(int data);
-    int popsum(int s);
-};
-
-arrayStack::arrayStack(int capacity) {
-    this->capacity = capacity;
-    this->arr = new int[capacity];
-    topIndex = -1;
-}
-
-bool arrayStack::empty() {
-    if(topIndex == -1) {
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-int arrayStack::size() {
-    return topIndex + 1;
-}
-
-int arrayStack::top() {
-    if(empty()) {
-        return -1;
-    }
-    return arr[topIndex];
-}
-
-void arrayStack::push(int data) {
-    if(size() >= capacity) {
-        cout << "FULL";
-    }
-    else{
-        arr[topIndex + 1] = data;
-        topIndex++;
-    }
-}
-
-int arrayStack::popsum(int s) {
-    int sum = 0;
-    if(empty()) {
-        return -1;
-    }
-    else if(s > size()) {
-        for(int i = 0; i < size(); i++) {
-            sum += arr[i];
-        }
+    arrayStack(int sz) {
+        capacity = sz;
         topIndex = -1;
-        return sum;
+        arr = new int[capacity];
     }
-    else{
-        for(int i = 0; i < s; i++) {
-            sum += arr[topIndex-i];
-        }
-        topIndex-=s;
-        return sum;
-    }
-}
-
-int main() {
-    int t,N,data,s;
-    string command;
-    cin >> t >> N;
-    arrayStack a(t);
-
-    while(N--) {
-        cin >> command;
-        if(command == "empty") {
-            cout << a.empty() << endl;
-        }
-        else if(command == "size") {
-            cout << a.size() << endl;
-        }
-        else if(command == "push") {
-            cin >> data;
-            a.push(data);
-        }
-        else if(command == "popsum") {
-            cin >> s;
-            cout << a.popsum(s) << endl;
+    bool empty() {
+        if(topIndex == -1) {
+            return true;
         }
         else{
-            cout << a.top() << endl;
+            return false;
         }
     }
-    return 0;
+
+    int size() {
+        return topIndex + 1;
+    }
+
+    int top() {
+        if(empty()) {
+            return -1;
+        }
+        else{return arr[topIndex];}
+    }
+
+    void push(int x) {
+        if(capacity <= size()) {
+            cout << "FULL" << endl;
+            return;
+        }
+        arr[topIndex+=1] = x;
+    }
+
+    void pop() {
+        if(empty()) {
+            return;
+        }
+        topIndex--;
+    }
+    int popsum(int s) {
+        if(empty()) {
+            return -1;
+        }
+        else {
+            int total = 0;
+            for(int i = 0; i < s; i++) {
+                total += top();
+                pop();
+                if(empty()) {
+                    break;
+                }
+            }
+            return total;
+        }
+    }
+};
+
+int main() {
+    int t,n; string command;
+    cin >> t >> n;
+    arrayStack st(t);
+    while(n--) {
+        cin >> command;
+        if(command == "empty") {
+            if(st.empty()) {
+                cout << 1 << endl;
+            }
+            else{
+                cout << 0 << endl;
+            }
+        }
+        else if(command == "size") {
+            cout << st.size() << endl;
+        }
+        else if(command == "top") {
+            cout << st.top() << endl;
+        }
+        else if(command == "push") {
+            int x;
+            cin >> x;
+            st.push(x);
+        }
+        else if(command == "popsum") {
+            int s;
+            cin >> s;
+            cout << st.popsum(s) << endl;
+        }
+    }
 }
