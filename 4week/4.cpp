e339f4cc026be7056aec09455f6b939861550491
LeeJaeJun1 <jj030207@naver.com>
2025-02-19 18:33:13 +0900

    4week

--- 4week/4.cpp
+++ 4week/4.cpp
@@ -1,20 +1,25 @@
 #include<iostream>
 using namespace std;
 
-
-class listQueue{
+class arrayQueue{
 private:
     int* arr;
-    int size;
+    int capacity;
     int front;
     int rear;
-    int capacity;
+    int size;
 public:
-    listQueue(int c) {
+    arrayQueue(int c) {
         capacity = c;
-        arr = new int[capacity];
         front = rear = 0;
         size = 0;
+        arr = new int[capacity];
+    }
+    arrayQueue() {
+        capacity = 100;
+        front = rear = 0;
+        size=0;
+        arr = new int[capacity];
     }
     bool empty() {
         if(size==0) {
@@ -22,21 +27,50 @@ public:
         }
         return false;
     }
-    int getfront() {
-        if(empty())
-            return -1;
-        return arr[front];
+    int Size() {
+        return size;
+    }
+    void full() {
+        if(capacity <= Size()) {
+            cout << "True" << endl;
+        }
+        else{
+            cout << "False" << endl;
+        }
+    }
+    void Front() {
+        if(empty()) {
+            cout << "Empty" << endl;
+        }
+        cout << arr[front] << endl;
     }
 
-    void enqueue(int v) {
-        if(capacity<=size) {
+    void rearNsum(int num) {
+        if(empty()) {
+            cout << "Empty" << endl;
+            return;
+        }
+        if(Size() < num) {
+            cout << "error" << endl;
+            return;
+        }
+        int total = 0;
+        for(int i = num; i > 0; i--) {
+            total += arr[(rear-i + capacity) % capacity];
+        }
+        cout << total << endl;
+    }
+
+    void enqueue(int value) {
+        if(capacity <= size) {
             cout << "Full" << endl;
             return;
         }
-        arr[rear] = v;
+        arr[rear] = value;
         rear = (rear+1) % capacity;
         size++;
     }
+
     void dequeue() {
         if(empty()) {
             cout << "Empty" << endl;
@@ -45,77 +79,78 @@ public:
         front = (front+1) % capacity;
         size--;
     }
-    void plusfront(int n) {
-        if(empty()) {
-            return;
-        }
-        arr[front]+=n;
+    int getFront() {
+        return arr[front];
+    }
+    void plusFront(int n) {
+        arr[front] += n;
     }
 };
 
 int main() {
-    int test, card, num,diff,last;
-    cin >> test;
-    while(test--) {
-        int score1 = 0; int score2 = 0;
-        cin >> card;
-        listQueue l1(card);
-        listQueue l2(card);
-        for(int i = 0 ; i < card; i++) {
-            cin >> num;
-            l1.enqueue(num);
+    int T,N;
+    cin >> T;
+    while(T--) {
+        cin >> N;
+        arrayQueue a1;
+        arrayQueue a2;
+        int total1 = 0; int total2 = 0;
+        int x1,y1,diff,last;
+        for(int i = 0; i < N; i++) {
+            cin >> x1;
+            a1.enqueue(x1);
         }
-        for(int i = 0; i < card; i++) {
-            cin >> num;
-            l2.enqueue(num);
+        for(int i = 0; i < N; i++) {
+            cin >> y1;
+            a2.enqueue(y1);
         }
-        for(int i = 1; i <= card; i++) {
-            if(l1.getfront() > l2.getfront()) {
-                score1++;
-                cout << "Round" << i << " " << l1.getfront() << ">" << l2.getfront() << " " <<  score1 << ":" << score2 << endl;
-                diff = l1.getfront() - l2.getfront();
-                l1.dequeue();
-                l2.dequeue();
-                l2.plusfront(diff);
-                if(i==card) {
+        for(int j = 1; j <= N; j++) {
+            if(a1.getFront() > a2.getFront()) {
+                total1++;
+                cout << "Round" << j << " " << a1.getFront() << ">" << a2.getFront()
+                     << " " << total1 << ":" << total2 << endl;
+                diff = a1.getFront() - a2.getFront();
+                a1.dequeue(); a2.dequeue();
+                a2.plusFront(diff);
+                if(j==N) {
                     last = 1;
                 }
             }
-            else if(l2.getfront() > l1.getfront()) {
-                score2++;
-                cout << "Round" << i << " " << l1.getfront() << "<" << l2.getfront() << " " <<score1 << ":" << score2 << endl;
-                diff = l2.getfront() - l1.getfront();
-                l1.dequeue();
-                l2.dequeue();
-                l1.plusfront(diff);
-                if(i==card) {
+            else if(a1.getFront() < a2.getFront()) {
+                total2++;
+                cout << "Round" << j << " " << a1.getFront() << "<" << a2.getFront()
+                     << " " << total1 << ":" << total2 << endl;
+                diff = a2.getFront() - a1.getFront();
+                a1.dequeue(); a2.dequeue();
+                a1.plusFront(diff);
+                if(j==N) {
                     last = 2;
                 }
             }
             else{
-                cout << "Round" << i << " " << l1.getfront() << "=" << l2.getfront() << " " << score1 << ":" << score2 << endl;
-                l1.dequeue();
-                l2.dequeue();
-                if(i==card) {
+                cout << "Round" << j << " " << a1.getFront() << "=" << a2.getFront()
+                     << " " << total1 << ":" << total2 << endl;
+                a1.dequeue(); a2.dequeue();
+                if(j==N) {
                     last = 0;
                 }
             }
         }
-        if(score1 > score2) {
+        if(total1 > total2) {
             cout << "Winner P1" << endl;
         }
-        else if(score2 > score1) {
+        else if(total1 < total2) {
             cout << "Winner P2" << endl;
         }
         else{
-            if(last==0) {
-                cout << "Draw" << endl;
-            }
-            else if(last==1) {
+            if(last==1) {
                 cout << "Winner P1" << endl;
             }
-            else if(last==2) {
-                cout << "Winner p2" << endl;
+            else if(last == 2) {
+                cout << "Winner P2" << endl;
+            }
+            else{
+                cout << "Draw" << endl;
             }
         }
     }
