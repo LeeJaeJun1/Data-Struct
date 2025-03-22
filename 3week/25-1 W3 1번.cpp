396971987572a666fc23ad230f5ccd30df8a32d4
LeeJaeJun1 <jj030207@naver.com>
2025-03-22 21:35:36 +0900

    3week

---
+++ 3week/25-1 W3 1번.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+
+class arrayStack{
+private:
+    int capacity;
+    int topIndex;
+    int* arr;
+public:
+    arrayStack() {
+        capacity = 100000;
+        topIndex = -1;
+        arr = new int[capacity];
+    }
+    int size() {
+        return topIndex+1;
+    };
+    bool empty() {
+        return topIndex==-1;
+    }
+    int top() {
+        if(empty()) {
+            return -1;
+        }
+        return arr[topIndex];
+    }
+    void push(int x) {
+        arr[topIndex+=1] = x;
+    }
+    void pop() {
+        if(empty()) {
+            cout << -1 << "\n";
+            return;
+        }
+        cout << top() << "\n";
+        topIndex--;
+    }
+    void pop_all() {
+        if(empty()) {
+            cout << -1 << "\n";
+            return;
+        }
+
+        while(!empty()) {
+            cout << top() << " ";
+            topIndex--;
+        }
+        cout << "\n";
+    }
+};
+
+int main() {
+    int t,q; string s;
+    cin >> t;
+    while(t--) {
+        arrayStack sta;
+        cin >> q;
+        while(q--) {
+            cin >> s;
+            if(s=="size") {
+                cout << sta.size() << "\n";
+            }
+            else if(s=="empty") {
+                if(sta.empty()) {
+                    cout << 1 << "\n";
+                }
+                else{
+                    cout << 0 << "\n";
+                }
+            }
+            else if(s=="top") {
+                cout << sta.top() << "\n";
+            }
+            else if(s=="push") {
+                int x; cin >> x;
+                sta.push(x);
+            }
+            else if(s=="pop") {
+                sta.pop();
+            }
+            else if(s=="pop_all") {
+                sta.pop_all();
+            }
+        }
+    }
+}
