9672ac9e5ed3c844c5804699d83539d524416994
LeeJaeJun1 <jj030207@naver.com>
2025-03-30 17:50:04 +0900

    4week

---
+++ 4week/25-1 W4 3번.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+
+class arrayQueue {
+private:
+    int* arr;
+    int front;
+    int rear;
+    int capacity;
+    int n;
+public:
+    arrayQueue() {
+        capacity = 10000;
+        front = rear = 0;
+        n = 0;
+        arr = new int[capacity];
+    }
+    bool empty() {
+        return n == 0;
+    }
+    int size() {
+        return n;
+    }
+    void Front() {
+        if(empty()) {
+            cout << -1 << "\n";
+            return;
+        }
+        cout << arr[front] << "\n";
+    }
+    void push(int x) {
+        arr[rear] = x;
+        rear = (rear+1) % capacity;
+        n++;
+    }
+    void pop() {
+        if(empty()) {
+            cout << -1 << "\n";
+            return;
+        }
+        cout << arr[front] << "\n";
+        front = (front + 1) % capacity;
+        n--;
+    }
+    void print_all() {
+        if(empty()) {
+            cout << -1 << "\n";
+            return;
+        }
+        for(int i = front; i < rear; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+};
+
+int main() {
+    int t,q; string comm;
+    cin >> t;
+    while(t--) {
+        arrayQueue qu;
+        cin >> q;
+        while(q--) {
+            cin >> comm;
+            if(comm == "empty") {
+                if(qu.empty()) {
+                    cout << 1 << "\n";
+                }
+                else{
+                    cout << 0 << "\n";
+                }
+            }
+            else if(comm == "size") {
+                cout << qu.size() << "\n";
+            }
+            else if(comm=="front") {
+                qu.Front();
+            }
+            else if(comm=="push") {
+                int x; cin>> x;
+                qu.push(x);
+            }
+            else if(comm=="pop") {
+                qu.pop();
+            }
+            else if(comm=="print_all") {
+                qu.print_all();
+            }
+        }
+    }
+}
+
